96163ab50a39acff178db1fc2ef4653d19e2f69a
Alex Novak <alexleonnovak@gmail.com>
2022-06-13 10:00:52 +0300

    log

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const { main } = require("./src/functions");
+console.log('DIR: ', __dirname);
 // (async () => await main())();
 // process.exit(1);
 module.exports = { main };
